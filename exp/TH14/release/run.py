cce9d4204aa3ad9b22531bbd27395cc0b150278a
zhengshou <zs2262@columbia.edu>
2018-12-21 17:37:11 -0500

    update exp dir

--- exp/TH14/release/run.py
+++ exp/TH14/release/run.py
@@ -2,8 +2,8 @@ import os
 import numpy as np
 import yaml
 
-EXP_NAME='AN'
-EXP_PATH='exp/'+EXP_NAME+'/'
+EXP_NAME='release'
+EXP_PATH='exp/TH14/'+EXP_NAME+'/'
 
 def rewrite_cfg(tmp,tmpvalue):
 	cmd = 'sed -E -i "s/'+tmp+': .*$/'+tmp+': '+str(tmpvalue)+'/g" "'+EXP_PATH+'config.yml"'
@@ -19,32 +19,32 @@ def rewrite_solver(tmp,tmpvalue):
 	cmd = 'sed -E -i "s/'+tmp+': .*$/'+tmp+': '+str(tmpvalue)+'/g" "'+EXP_PATH+'solver.tpl"'
 	#print cmd
 	os.system(cmd)
-	
-MAX_ITER = 4706
+
+MAX_ITER = 200
 num_process = 24
 
 rewrite_cfg('MAX_ITER',MAX_ITER)
 os.system('pwd')
 
 # train on multiple videos
-rewrite_cfg_str('DATA_FILE','att_unfused_onlytrain\/train.all.tsv')
-rewrite_cfg_str('STAGE','train')
+rewrite_cfg_str('DATA_FILE','att_unfused\/val.all.tsv')
+rewrite_cfg_str('STAGE','val')
 rewrite_solver('snapshot',MAX_ITER)
-cmd1 = 'python tools/proc_net.py --phase train --dataset AN --expname '+EXP_NAME+' --rsltname '\
+cmd1 = 'python tools/proc_net.py --phase train --dataset TH14 --expname '+EXP_NAME+' --rsltname '\
 +'"tmp"'
 os.system(cmd1)
 
 # test & eval on all testing videos. prll mode.
-rewrite_cfg_str('DATA_FILE','att_unfused_onlytrain\/val.all.tsv')
-rewrite_cfg_str('STAGE','val')
+rewrite_cfg_str('DATA_FILE','att_unfused\/test.all.tsv')
+rewrite_cfg_str('STAGE','test')
 rewrite_cfg('MAX_ITER',MAX_ITER)
-cmd2 = 'python tools/proc_prll_net.py --phase test --dataset AN --expname '+EXP_NAME+' --rsltname '\
+cmd2 = 'python tools/proc_prll_net.py --phase test --dataset TH14 --expname '+EXP_NAME+' --rsltname '\
 +'"test_rslt'+'_trainnms'+str(0.4)+'_fg'+str(0.1)+'_ioc'+str(0.7)\
 +'_testnms'+str(0.4)+'"'+' --num_workers '+str(num_process)\
 +' --pretrained autoloc_iter_'+str(MAX_ITER)+'.caffemodel'
 os.system(cmd2)
 # single nms eval
-cmd3 = 'python tools/eval_metrics.py --phase test --dataset AN --expname '+EXP_NAME+' --rsltname '\
+cmd3 = 'python tools/eval_metrics.py --phase test --dataset TH14 --expname '+EXP_NAME+' --rsltname '\
 +'"test_rslt'+'_trainnms'+str(0.4)+'_fg'+str(0.1)+'_ioc'+str(0.7)\
 +'_testnms'+str(0.4)+'"'+' --num_workers '+str(num_process)
 os.system(cmd3)
